1ddaf69dd751f670676d7401c097ad32118e7a8c
Ramires00 <91140243+Ramires00@users.noreply.github.com>
2022-08-25 23:44:23 -0300

    controller tirando fotos

--- lib/camera_controller.dart
+++ lib/camera_controller.dart
@@ -1,12 +1,39 @@
+import 'dart:typed_data';
+
 import 'package:camera/camera.dart';
 import 'package:get/get.dart';
 
 class ControllerCamera extends GetxController {
+  @override
+  void dispose() {
+    super.dispose();
+    controller.value?.dispose();
+  }
+
+  @override
+  onReady() {
+    isCameraStarting.value = true;
+    cameraInit().catchError(onCameraError).whenComplete(() {
+      controller.value = CameraController(
+        _cameras[0],
+        ResolutionPreset.max,
+      );
+      isCameraStarting.value = false;
+    });
+    super.onReady();
+  }
+
   Rx<CameraController?> controller = Rx(null);
   late List<CameraDescription> _cameras;
 
+  RxBool isCameraInUse = RxBool(false);
+  RxBool isCameraLoading = RxBool(false);
+  RxBool isCameraStarting = RxBool(false);
+
+  Rx<Uint8List> file = Uint8List(0).obs;
+
   Future<void> cameraInit() async {
-    _cameras = await availableCameras();
+    _cameras = await availableCameras().catchError(onCameraError);
   }
 
   onCameraError(Object error) {
@@ -22,20 +49,22 @@ class ControllerCamera extends GetxController {
     }
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-    controller.value?.dispose();
-  }
+  Future<void> takePicture() async {
+    if (controller.value?.value.isTakingPicture ?? true) {
+      print("======== CAMERA IS BUSY. CANCELLING... ========");
+      return;
+    }
 
-  @override
-  onReady() {
-    super.onReady();
-    cameraInit().whenComplete(() {
-      controller.value = CameraController(
-        _cameras[1],
-        ResolutionPreset.max,
-      );
-    });
+    try {
+      print("======== TAKING PHOTO... ========");
+      file.value = await controller.value
+              ?.takePicture()
+              .then((value) async => await value.readAsBytes()) ??
+          await XFile("").readAsBytes();
+      print("======== PHOTO TAKEN SUCCESSFULLY ========");
+      print("======== PHOTO PATH ${file.value.toString()}");
+    } catch (err) {
+      rethrow;
+    }
   }
 }
